26c959586aeeb4fc97bfc0bfe0e5c9cba9025aa2
prashanthseshadri <sp.1990@gmail.com>
2012-06-02 00:02:29 +0530

    Updated the termina and started with autocomplete
    Just movked version.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 var cursor = null;
 var blinkingFunction;
+function setTextFocus()
+{
+	document.querySelector('#terminal-input.selected').focus();
+
+}
 function initialize()
 {
 	document.onkeypress = keyCapture;
+	$('#terminal-input').live('keydown',processInput)
+	setTextFocus();
 	blinkCursor();
 }
 function blinkCursor()
@@ -37,5 +44,29 @@ function displayAlert()
 
 function keyCapture(e)
 {
-	displayAlert();
+	if(e.keyCode==13)
+	{
+//		displayAlert();		
+	}
+}
+
+function processInput(e)
+{
+	var temp = document.querySelector('#terminal-display.selected');
+	var currentTextBox = temp.childNodes[5]; 
+	if(e.keyCode==13)
+	{
+		var newNode = "<p1 id=\"terminal-display\" class=\"selected\">"+temp.innerHTML+"</p1>";
+		temp.setAttribute("class","unselected");
+		currentTextBox.setAttribute('class','unselected');
+		currentTextBox.setAttribute('value',currentTextBox.value);
+		currentTextBox.setAttribute('readonly','true');
+		document.querySelector('#main').innerHTML = document.querySelector('#main').innerHTML + "<br>" + newNode;
+		setTextFocus();
+	}
+	else if(e.keyCode==9)
+	{
+		e.preventDefault();
+		currentTextBox.value = "Auto Completion to be done..";
+	}
 }
