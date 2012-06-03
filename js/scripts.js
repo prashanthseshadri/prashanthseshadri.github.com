cfbc059262c2d5146ed9aad42fc7a6d61c9cbfe5
prashanthseshadri <sp.1990@gmail.com>
2012-06-03 20:46:48 +0530

    Console and IMDB
    Console output added and also IMDB query(thanks radha) been added .

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var cursor = null;
+var blinkingFunction;
 function setTextFocus()
 {
 	document.querySelector('#terminal-input.selected').focus();
@@ -6,8 +7,10 @@ function setTextFocus()
 }
 function initialize()
 {
+	document.onkeypress = keyCapture;
 	$('#terminal-input').live('keydown',processInput)
 	setTextFocus();
+//	blinkCursor();
 }
 function displayAlert()
 {
@@ -15,6 +18,17 @@ function displayAlert()
 	if(progressAlert.innerHTML.length==0)
 	{
 		progressAlert.innerHTML = "Under Construction..Try After months :P";
+	}
+	/*var progressAlert = document.querySelector("p");
+	progressAlert.innerHTML = progressAlert.innerHTML + "<br>" + "Hello";*/
+	
+}
+
+function keyCapture(e)
+{
+	if(e.keyCode==13)
+	{
+//		displayAlert();		
 	}
 }
 
@@ -69,6 +83,12 @@ function unsetActiveElements()
 }
 function createNewNodeAndAppend()
 {
+	/*var newNode = "<p id=\"terminal-display\" class=\"selected\">"+temp.innerHTML+"</p>";
+	temp.setAttribute("class","unselected");
+	currentTextBox.setAttribute('class','unselected');
+	currentTextBox.setAttribute('value',currentTextBox.value);
+	currentTextBox.setAttribute('readonly','true');
+	document.querySelector('#main').innerHTML = document.querySelector('#main').innerHTML + "<br>" + newNode;*/
 	//Don forget to unset the active elements before apending the new active elements
 	unsetActiveElements();
 	var newnode = createNewNode();
@@ -87,12 +107,92 @@ function writeOutput(parentNode,childNode)
 		return;
 	parentNode.appendChild(childNode);
 }
+
+function showUsage(command)
+{
+	var commandList = commands;
+	var outputBlockContainer = document.querySelector("#output.selected");
+	var commandNode = document.createElement("table");
+	commandNode.className = "table-output";
+	if(command=="")
+	{
+		//list all usages
+		for(var i=0;i<commandList.length;i++)
+		{
+			commandNode.innerHTML +="<tr><td>" + commandList[i].name + "</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + commandList[i].description + "</td></tr>";
+			if(commandList[i].usage!=undefined)
+				commandNode.innerHTML +="<tr><td><i>Usage :</i> " + commandList[i].usage + "</td></tr>";
+		} 
+	}
+	else
+	{
+		for(var i=0;i<commandList.length;i++)
+		{
+			if(commandList[i].name == command && commandList[i].usage!=undefined)
+				commandNode.innerHTML +="<tr><td><i>Usage : </i>" + commandList[i].usage + "</td></tr>";
+		}
+	}
+	outputBlockContainer.appendChild(commandNode);
+	createNewNodeAndAppend();
+}
+function clearConsole()
+{
+	document.querySelector("#console").innerHTML = "Console Output : <br><span style='float:right;' onclick='clearConsole()'>Clear</a>";
+	//once a click is made - the textbox focus will be out. So resetting it.
+	setTextFocus();
+}
+function updateConsole(output)
+{
+	document.querySelector("#console").innerHTML += output + "<br>";
+}
+function imdb(movieName)
+{
+	updateConsole("Fetching Movie Review from IMDB..");
+	$.ajax({
+  	url: 'http://www.imdbapi.com/?i=&t='+movieName,
+  	success: function(responseText) 
+  	{
+		// Parsed JSON data to MovieData
+		var MovieData = eval("(" + responseText + ")");
+		
+		var MovieTitle=MovieData.Title;
+		var Year=MovieData.Year;
+		var Director=MovieData.Director;
+		var Writer=MovieData.Writer;
+		var Actors=MovieData.Actors;
+		var Plot=MovieData.Plot;
+		var Rating=MovieData.imdbRating;
+	
+		var commandNode=document.createElement("table");
+		commandNode.className = "movie-output";
+		var starttag="<tr><td>";var endtag="</td></tr>";
+		
+		commandNode.innerHTML +=starttag+" MovieTitle : "+MovieTitle+endtag;
+		commandNode.innerHTML +=starttag+" Year : "+Year+endtag;
+		commandNode.innerHTML +=starttag+" Director : "+Director+endtag;
+		commandNode.innerHTML +=starttag+" Writer : "+Writer+endtag;
+		commandNode.innerHTML +=starttag+" Actors : "+Actors+endtag;
+		commandNode.innerHTML +=starttag+" Plot : "+Plot+endtag;
+		commandNode.innerHTML +=starttag+" Rating : "+Rating+endtag;
+		var outputBlockContainer = document.querySelector("#output.selected");
+		outputBlockContainer.appendChild(commandNode);
+		createNewNodeAndAppend();
+  	}
+	});
+	
+
+}
 function processInput(e)
 {
 	var currentTextBox = document.querySelector("#terminal-input.selected");
-	var command = currentTextBox.value;
+	var input = currentTextBox.value;
+	var parameters = input.split(" ");
+	if(input.length==0)
+		return;
+	var command = parameters[0];
 	if(e.keyCode==13)
 	{
+		//enter key is pressed
 		switch(command)
 		{
 			case "clear":
@@ -100,16 +200,33 @@ function processInput(e)
 				clearScreen();
 				break;
 			}
+			case "imdb":
+			{
+				// command syntax  : imdb <movie-name>
+				var movieName = "";
+				for(var i=1;i<parameters.length;i++)
+					movieName +=parameters[i];
+				if(movieName.length==0)
+				{
+					showUsage(command);
+					break;
+				}
+				imdb(movieName);
+				break;
+			}
 			case "help":
 			{
-				var commandList = commands;
+				//empty parameter to showUsage will display all commands usages
+				command = "";
+				showUsage(command)
+				break;
+			}
+			case "fuck":
+			{
+				var commandNode=document.createElement("table");
+				commandNode.className="table-output";
 				var outputBlockContainer = document.querySelector("#output.selected");
-				var commandNode = document.createElement("table");
-				commandNode.className = "table-output";
-				for(var i=0;i<commandList.length;i++)
-				{
-					commandNode.innerHTML +="<tr><td>" + commandList[i].name + "</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + commandList[i].description + "</td></tr>";
-				}
+				commandNode.innerHTML+="<tr><td>"+"You should grow up buddy. "+command+ " in Windows !"+"</td></tr>";
 				outputBlockContainer.appendChild(commandNode);
 				createNewNodeAndAppend();
 				break;
@@ -128,7 +245,6 @@ function processInput(e)
 				writeOutput(outputBlockContainer,result);
 				createNewNodeAndAppend(); 
 			}
-			
 		}
 	}
 	else if(e.keyCode==9)
